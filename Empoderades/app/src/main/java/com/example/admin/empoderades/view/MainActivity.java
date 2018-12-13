9e2d075182adbcca7155a9bc68045f8db9b227d5
Tomi <tomas.canosa@hotmail.com>
2018-12-13 20:53:27 -0300

    botones para navegar por las pantallas

--- Empoderades/app/src/main/java/com/example/admin/empoderades/view/MainActivity.java
+++ Empoderades/app/src/main/java/com/example/admin/empoderades/view/MainActivity.java
@@ -11,9 +11,16 @@ import android.widget.TextView;
 import com.example.admin.empoderades.R;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private TextView newsButton;
     private TextView commonQuestionButton;
+    private TextView mapButton;
+    private TextView favouritesButton;
+    private TextView reportButton;
+    private List<TextView> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         newsButton = findViewById(R.id.news_button);
         commonQuestionButton = findViewById(R.id.common_question_button);
+        mapButton = findViewById(R.id.map_button);
+        favouritesButton = findViewById(R.id.favourites_button);
+        reportButton = findViewById(R.id.report_button);
+        buttons = new ArrayList<>();
+        buttons.add(newsButton);
+        buttons.add(commonQuestionButton);
+        buttons.add(mapButton);
+        buttons.add(favouritesButton);
+        buttons.add(reportButton);
 
         final FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -35,9 +51,9 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.mainFragmentContainer, new NewsFragment(), "tag");
                 fragmentTransaction.commit();
-                newsButton.setClickable(false);
-                commonQuestionButton.setClickable(true);
-
+                newsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                commonQuestionButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                buttonSelected(newsButton);
             }
         });
 
@@ -47,12 +63,42 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.mainFragmentContainer, new CommonQuestionsFragment(), "tag");
                 fragmentTransaction.commit();
-                commonQuestionButton.setClickable(false);
-                newsButton.setClickable(true);
-
-
+                buttonSelected(commonQuestionButton);
             }
         });
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSelected(mapButton);
+            }
+        });
+
+        favouritesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSelected(favouritesButton);
+            }
+        });
+
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSelected(reportButton);
+            }
+        });
+    }
+
+    private void buttonSelected (TextView selectedButton) {
+        for (TextView button : buttons) {
+            if (button != selectedButton){
+                button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                button.setClickable(true);
+            } else {
+                button.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                button.setClickable(false);
+            }
+        }
     }
 
 }
